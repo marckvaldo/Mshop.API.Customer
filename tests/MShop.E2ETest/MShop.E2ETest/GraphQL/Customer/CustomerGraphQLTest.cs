529c5edf3658ee731e7c077647d2ed13a2dffa37
marckvaldo <marckvaldo@hotmail.com>
2025-11-04 14:21:23 -0300

    chore: Create base structure for graphql customer tests

---
+++ tests/MShop.E2ETest/MShop.E2ETest/GraphQL/Customer/CustomerGraphQLTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MShop.E2ETest.GraphQL.Customer
+{
+    [Collection("API Customer GraphQL")]
+    [CollectionDefinition("API Customer GraphQL", DisableParallelization = true)]
+    public class CustomerGraphQLTest : CustomerGraphQLTestFixture
+    {
+        public CustomerGraphQLTest() : base()
+        {
+            
+        }
+
+        [Fact(DisplayName = nameof(GetCustomerById))]
+        [Trait("EndToEnd/API", "Customer - Endpoints")]
+        public void GetCustomerById()
+        {
+
+        }
+    }
+}
---
+++ tests/MShop.E2ETest/MShop.E2ETest/GraphQL/Customer/CustomerGraphQLTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using MShop.E2ETest.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MShop.E2ETest.GraphQL.Customer
+{
+    public class CustomerGraphQLTestFixture : BaseFixture
+    {
+        public CustomerGraphQLTestFixture() : base()
+        {
+            
+        }
+
+    }
+}
